ee1c6c03b33729b910ece6fb6ba25e08d49117d0
dm-marina <mysummer1404@gmail.com>
2021-10-23 22:09:22 +0300

    dm-marina

---
+++ js/script.js
@@ -0,0 +1,71 @@
+let a = +prompt('Enter something')
+// 1
+// a = 0 ? 'неверно' : 'верно';
+// alert(a);
+// 2
+// let aIf = a>0 ? 'верно' : 'неверно';
+// alert(aIf);
+// 3
+// let aIf= a<0 ? 'верно' : 'неверно';
+// alert(aIf);
+// 4
+// let aIf = a>=0 ? 'верно' : 'неверно';
+// alert(aIf);
+// 5
+// let aIf = a <= 0 ? 'верно' : 'неверно';
+// alert(aIf);
+// 6
+// let aIf = a !=0 ? 'верно' : 'неверно';
+// alert(aIf);
+ // 7
+//  let str =prompt("Input words");
+//  str = 'test' ? 'верно' : 'неверно';
+//  alert(str);
+// 8
+// a === 1 ? 'верно' : 'неверно';
+// alert(a);
+// 9
+// let aIf = 0<a<5 ? 'Верно' : 'Неверно';
+// alert(aIf);
+// 10
+// if(a===0 || a===2){
+//     a+=7
+//     alert(a);
+// } else{
+//     // a/10
+//     alert(a/10);
+// }
+// 11
+// let b = +prompt('Enter number')
+// if( a<=1 && b>=3){
+//     alert(a+b);
+// }else{
+//     alert(a-b);
+// }
+// 12
+// let b = +prompt('Enter number')
+// if(2<a<11 || 6<=b<14){
+//     alert("Верно");
+// } else{
+//     alert("Неверно");
+// }
+// 13
+// const num = +prompt( "Enter number from 1 to 4")
+// switch (num){
+//     case 1:{let result}
+//         result = "зима";
+//         alert(result);
+//         break;
+//     case 2:{let result} 
+//         result = "весна";
+//         alert(result);
+//         break;
+//     case 3:{let result}
+//         result = "лето";
+//         alert(result);
+//         break;
+//     case 4:{let result}
+//         result = "осень";
+//         alert(result);
+//         break;
+// }
